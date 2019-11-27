334cc82a2f7d1dd7fdee65b0a61897652f1f824b
Eduardo Chianelli <educhianelli@gmail.com>
2019-11-27 16:41:57 -0300

    Liberando o swagger do spring security

--- back-end/src/main/java/com/desafio/surittec/config/AuthorizationServerConfigUtil.java
+++ back-end/src/main/java/com/desafio/surittec/config/AuthorizationServerConfigUtil.java
@@ -1,7 +1,9 @@
 package com.desafio.surittec.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -13,5 +15,21 @@ public class AuthorizationServerConfigUtil extends WebSecurityConfigurerAdapter 
 	public AuthenticationManager authenticationManager() throws Exception {
 		return super.authenticationManager();
 	}
+	@Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+            .antMatchers(HttpMethod.OPTIONS, "/**")
+            .antMatchers("/app/**/*.{js,html}")
+            .antMatchers("/i18n/**")
+            .antMatchers("/content/**")
+            .antMatchers("/swagger-ui.html")
+            .antMatchers("/swagger-resources/**")
+            .antMatchers("/configuration/security")
+            .antMatchers("/webjars/**")
+            .antMatchers("/v2/api-docs/**")
+            .antMatchers("/configuration/ui/**")
+            .antMatchers("/antenas/**")
+            .antMatchers("/test/**");
+    }
 
 }
